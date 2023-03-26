7c4b8e06cfc0fcb635b2ec31f2934a4e3461872c
edu <edu@mail.pl>
2023-03-26 10:41:17 +0200

    configure mvn project and write first unit test

---
+++ src/test/java/pl/coderslab/seleniumcourseonlteaw35/JUnitExampleTest.java
@@ -0,0 +1,11 @@
+package pl.coderslab.seleniumcourseonlteaw35;
+
+import org.junit.jupiter.api.Test;
+
+public class JUnitExampleTest {
+    @Test
+    public void firstTest() {
+        // test code
+        System.out.println("firstTest");
+    }
+}
