7e4d977b8bf54fd582f9f41ddd97dd4dca4e5dad
edu <edu@mail.pl>
2023-03-26 12:03:09 +0200

    d4t first selenium test search with duckduckgo step 2

--- src/test/java/pl/coderslab/seleniumcourseonlteaw35/DuckDuckGoTest.java
+++ src/test/java/pl/coderslab/seleniumcourseonlteaw35/DuckDuckGoTest.java
@@ -3,14 +3,22 @@ package pl.coderslab.seleniumcourseonlteaw35;
 // tes https://duckduckgo.com/ search
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class DuckDuckGoTest {
     @Test
     public void searchWithDuckDuckGo() {
         WebDriver driver = new ChromeDriver();
         driver.get("https://duckduckgo.com/");
+        WebElement searchInput = driver.findElement(By.id("search_form_input_homepage"));
+        searchInput.sendKeys("W pustyni i w puszczy");
+        WebElement searchButton = driver.findElement(By.id("search_button_homepage"));
+        searchButton.click();
 //        driver.close();
     }
 }
