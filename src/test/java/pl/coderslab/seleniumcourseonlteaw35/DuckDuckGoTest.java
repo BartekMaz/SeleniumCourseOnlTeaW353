1e5e663bdd0b0b0a5f7c0634b400b79db4b31665
edu <edu@mail.pl>
2023-03-26 14:50:50 +0200

    d4t configure timeouts

--- src/test/java/pl/coderslab/seleniumcourseonlteaw35/DuckDuckGoTest.java
+++ src/test/java/pl/coderslab/seleniumcourseonlteaw35/DuckDuckGoTest.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.fail;
@@ -18,6 +19,7 @@ public class DuckDuckGoTest {
     public void searchWithDuckDuckGo() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(16));
         // given
         driver.get("https://duckduckgo.com/");
         // when
