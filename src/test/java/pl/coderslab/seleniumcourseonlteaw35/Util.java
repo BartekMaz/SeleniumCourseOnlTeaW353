1b8c739aba76e2142e91a64fcaec05085e663a72
edu <edu@mail.pl>
2023-03-26 16:55:37 +0200

    d4tz1 zad 1 functions and interaction with elements

--- src/test/java/pl/coderslab/seleniumcourseonlteaw35/Util.java
+++ src/test/java/pl/coderslab/seleniumcourseonlteaw35/Util.java
@@ -1,9 +1,19 @@
 package pl.coderslab.seleniumcourseonlteaw35;
 
+import org.openqa.selenium.WebElement;
+
 import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class Util {
     public static String randomEmail() {
         return UUID.randomUUID().toString() + "@mail.pl";
+    }
+
+    public static void assertVisibleAndEnabled(WebElement element) {
+        if (!element.isEnabled() || !element.isDisplayed()) {
+            fail("element not visible or not enabled");
+        }
     }
 }
